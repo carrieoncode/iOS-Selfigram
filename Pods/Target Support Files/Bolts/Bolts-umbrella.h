3116d8b8074bded4e5e886198926f0c92b609ed7
Sam Meech-Ward <sam@meech-ward.com>
2016-11-23 15:33:11 -0800

    Update the cocoapods file to use the most recent version of Parse

--- Pods/Target Support Files/Bolts/Bolts-umbrella.h
+++ Pods/Target Support Files/Bolts/Bolts-umbrella.h
@@ -3,12 +3,11 @@
 #import "BFCancellationToken.h"
 #import "BFCancellationTokenRegistration.h"
 #import "BFCancellationTokenSource.h"
-#import "BFDefines.h"
 #import "BFExecutor.h"
+#import "BFTask+Exceptions.h"
 #import "BFTask.h"
 #import "BFTaskCompletionSource.h"
 #import "Bolts.h"
-#import "BoltsVersion.h"
 
 FOUNDATION_EXPORT double BoltsVersionNumber;
 FOUNDATION_EXPORT const unsigned char BoltsVersionString[];
