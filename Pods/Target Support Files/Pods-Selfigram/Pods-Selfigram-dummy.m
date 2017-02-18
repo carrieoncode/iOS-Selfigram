7e45dd6f6acb966b52ea7cc9f1f9c4e3d74fbe77
Sam Meech-Ward <sam@meech-ward.com>
2017-02-18 13:21:02 -0800

    Add gitignore and remove the Pods from git

--- Pods/Target Support Files/Pods-Selfigram/Pods-Selfigram-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Selfigram : NSObject
-@end
-@implementation PodsDummy_Pods_Selfigram
-@end
--- Pods/Target Support Files/Pods-SelfigramUITests/Pods-SelfigramUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SelfigramUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SelfigramUITests
-@end
